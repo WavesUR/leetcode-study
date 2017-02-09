0c72a533a7280b11266f88922268e57a98e770ba
WavesUR <waves.ur.ee@gmail.com>
2017-02-08 20:08:21 -0500

    2_8_2

--- 235/src_2_7.cpp
+++ 235/src_2_7.cpp
@@ -18,7 +18,13 @@ using namespace std;
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        if(p->val > root->val && q->val > root->val){
+        	return lowestCommonAncestor(root->right,p,q);
+        }
+        if(p->val < root->val && q->val < root->val){
+        	return lowestCommonAncestor(root->left,p,q);
+        }
+        return root;
     }
 };
 
