b784b8af26fda2e8c5cec45e83835d2ea2d0e661
WavesUR <waves.ur.ee@gmail.com>
2017-02-22 17:26:33 -0500

    2_22

--- 151/src.cpp
+++ 151/src.cpp
@@ -13,23 +13,42 @@ using namespace std;
 class Solution {
 public:
     void reverseWords(string &s) {
-        int start = 0;
-        int end = s.length() - 1;
-        while(start < end){
-            swap(s[start],s[end]);
-            start++;
-            end--;
+        int len = s.length();
+        int slow = 0;
+        int fast = 1;
+        while(fast < len){
+            while(fast < len && !(s[fast] == ' ' && s[fast-1] != ' ')){
+                fast++;
+            }
+            int fast_temp = fast - 1;
+            int slow_temp = slow;
+            while(slow_temp < fast_temp){
+                swap(s[slow_temp],s[fast_temp]);
+                slow_temp++;
+                fast_temp--;
+            }
+            while(slow < len && !(s[slow] == ' ' && s[slow-1] != ' ')){
+                slow++;
+            }
+             slow = slow + 1;
+             fast = fast + 1;
         }
+        while(s[len-1] == ' '){
+            len--;
+        }
+        s = s.substr(0,len);
+        reverse(s.begin(),s.end());
     }
 };
 
 int main(){
 
-    string s = "the sky is blue";
+    string s = " ";
+    cout << s << "." <<endl;
     string result;
     Solution solution;
     solution.reverseWords(s);
-    cout << s << endl; 
+    cout << s << "." <<endl; 
 
 
     
---
+++ 155/src_2_22.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <stack>
+#include <queue>
+using namespace std;
+
+
+class MinStack {
+public:
+    stack<int> s;
+    vector<pair<int,int> > min_val;
+    /** initialize your data structure here. */
+    MinStack() {
+        while(!s.empty()){
+            s.pop();
+        }
+        min_val = {};
+    }
+    
+    void push(int x) {
+
+        if(s.empty() || x < min_val.back().first){
+            min_val.push_back(make_pair(x,min_val.size()+1));
+        }
+        s.push(x);        
+    }
+    
+    void pop() {
+        s.pop();
+        if(s.size() < min_val.back().second){
+            min_val.pop_back();
+        }
+        
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+       return min_val.back().first; 
+    }
+};
+
+int main(){
+
+ int x = 9;
+ MinStack obj;
+
+ obj.push(x);
+// obj.pop();
+ // int param_3 = obj.top();
+ // int param_4 = obj.getMin();
+ 
+}
